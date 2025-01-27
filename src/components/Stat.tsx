2ff888332e9b16089192e70489b0fec69480b39c
prevail <enitan.ayoola@gmail.com>
2025-01-27 14:19:13 +0100

    create the stat  components

--- src/components/Stat.tsx
+++ src/components/Stat.tsx
@@ -1,7 +1,8 @@
-import React from "react";
-
-const Stat = () => {
-  return <div>Stat</div>;
-};
+const Stat = ({ label, value }: { label: string; value: number }) => (
+  <div className="text-center sm:text-left">
+    <p className="text-[13px] opacity-75">{label}</p>
+    <p className="text-[22px] font-bold">{value.toLocaleString()}</p>
+  </div>
+);
 
 export default Stat;
