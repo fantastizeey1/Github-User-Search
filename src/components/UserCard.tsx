9d2abcb7fdb524db199889098410cdb197903478
prevail <enitan.ayoola@gmail.com>
2025-01-27 14:16:06 +0100

    create the user card components that displays the fetched user info

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,7 +1,73 @@
-import React from "react";
+import formatDate from "../utils/formatDate";
+import Stat from "./Stat";
+import InfoItem from "./InfoItem";
+import { MapPin, Link as LinkIcon, Twitter, Building } from "lucide-react";
 
-const UserCard = () => {
-  return <div>UserCard</div>;
-};
+const UserCard = ({ user, darkMode }: { user: any; darkMode: boolean }) => (
+  <div
+    className={`p-6 sm:p-8 rounded-[15px] shadow-md ${
+      darkMode ? "bg-card-dark" : "bg-card-light"
+    }`}
+  >
+    <div className="flex gap-6 sm:gap-8">
+      <img
+        src={user.avatar_url}
+        alt={user.name}
+        className="w-[70px] h-[70px] sm:w-[117px] sm:h-[117px] rounded-full"
+      />
+      <div className="flex-1 space-y-6">
+        <div className="flex flex-col sm:flex-row sm:justify-between gap-2">
+          <div>
+            <h2 className="text-h2 text-[26px] font-bold">
+              {user.name || user.login}
+            </h2>
+            <p className="text-primary">@{user.login}</p>
+          </div>
+          <p className="text-body opacity-75">
+            Joined {formatDate(user.created_at)}
+          </p>
+        </div>
+        <p className="text-body">{user.bio || "This profile has no bio"}</p>
+        <div
+          className={`grid grid-cols-3 gap-4 p-4 sm:p-6 rounded-[10px] ${
+            darkMode ? "bg-dark" : "bg-light"
+          }`}
+        >
+          <Stat label="Repos" value={user.public_repos} />
+          <Stat label="Followers" value={user.followers} />
+          <Stat label="Following" value={user.following} />
+        </div>
+        <div className="grid sm:grid-cols-2 gap-4">
+          <InfoItem
+            icon={<MapPin />}
+            text={user.location}
+            available={!!user.location}
+          />
+          <InfoItem
+            icon={<LinkIcon />}
+            text={user.blog}
+            link={user.blog}
+            available={!!user.blog}
+          />
+          <InfoItem
+            icon={<Twitter />}
+            text={`@${user.twitter_username}`}
+            available={!!user.twitter_username}
+            link={
+              user.twitter_username
+                ? `https://twitter.com/${user.twitter_username}`
+                : undefined
+            }
+          />
+          <InfoItem
+            icon={<Building />}
+            text={user.company}
+            available={!!user.company}
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 export default UserCard;
