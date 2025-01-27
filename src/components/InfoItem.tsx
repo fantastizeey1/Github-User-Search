cfec9038c1144f8f3e5fa79361cfc65802bf6b0a
prevail <enitan.ayoola@gmail.com>
2025-01-27 13:18:55 +0100

    create the components needed to build the project

---
+++ src/components/InfoItem.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const InfoItem = () => {
+  return <div>InfoItem</div>;
+};
+
+export default InfoItem;
---
+++ src/pages/GithubUserSearch.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GithubUserSearch = () => {
+  return <div>GithubUserSearch</div>;
+};
+
+export default GithubUserSearch;
